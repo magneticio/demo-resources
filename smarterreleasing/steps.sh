8bbfdfeee33fe1fd04b0d1cc4b5c1db942f275d7
berkgokden <berkgokden@gmail.com>
2019-01-21 16:56:49 +0100

    Update steps.sh for new app name

--- smarterreleasing/steps.sh
+++ smarterreleasing/steps.sh
@@ -1,21 +1,21 @@
 #!/usr/bin/env bash
 
 URL=$1
-vamp2cli --login http://$URL --user root --password root
+vamp login --url http://$URL --user root --password root
 PROJECT_NAME=sava
-vamp2cli create project $PROJECT_NAME --init
-vamp2cli create user user1 -f https://raw.githubusercontent.com/magneticio/demo-resources/master/vamplamiacliv1/user1
+vamp create project $PROJECT_NAME --init
+vamp create user user1 -f https://raw.githubusercontent.com/magneticio/demo-resources/master/vamplamiacliv1/user1
 .yaml
-vamp2cli grant --user user1 --role admin -p $PROJECT_NAME
-vamp2cli login --user user1 --password pass1
-vamp2cli bootstrap cluster prod
+vamp grant --user user1 --role admin -p $PROJECT_NAME
+vamp login --user user1 --password pass1
+vamp bootstrap cluster prod
 kubectl get pods -n vamp-system
 kubectl apply -f sava-namespace.yml
-vamp2cli list virtualclusters
+vamp list virtualclusters
 kubectl apply -f sava-cart-1.0.5-ie.yml
-vamp2cli create gateway sava-gateway -f ./sava-gateway.yaml
-vamp2cli create destination sava-cart-destination -f ./sava-cart-destination.yaml
-vamp2cli create vampservice sava-cart-vampservice -f ./sava-cart-vampservice.yaml
-GATEWAY_IP=$(vamp2cli get gateway sava-gateway -o=json --jsonpath '$.status.ip' --wait)
+vamp create gateway sava-gateway -f ./sava-gateway.yaml
+vamp create destination sava-cart-destination -f ./sava-cart-destination.yaml
+vamp create vampservice sava-cart-vampservice -f ./sava-cart-vampservice.yaml
+GATEWAY_IP=$(vamp get gateway sava-gateway -o=json --jsonpath '$.status.ip' --wait)
 echo $GATEWAY_IP
 curl -H "Host: ie.tutorials.vamp.cloud" -v http://$GATEWAY_IP
